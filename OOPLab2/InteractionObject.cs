59c46416c7f200ea48c6443f8722e36bc3b97c5c
klesogor <lesogor.kirill@gmail.com>
2018-12-15 18:37:35 +0200

    Fixed dependencies

--- OOPLab2/InteractionObject.cs
+++ OOPLab2/InteractionObject.cs
@@ -4,10 +4,6 @@ using OOPLab2.IO;
 using OOPLab2.Services;
 using OOPLab2.Services.Strategies;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOPLab2
 {
